e41caec1833313bff567ce51626dc1e3d1c520c4
stephmou <stephmou2@yahoo.com>
2019-08-27 10:54:06 -0400

    upload

---
+++ src/main/java/com/example/demo/Student.java
@@ -0,0 +1,41 @@
+package com.example.demo;
+
+public class Student {
+  private String name;
+  private String age;
+  private int height;
+  private int number;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
